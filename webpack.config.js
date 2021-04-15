fb80c432e3c43f629ce9c70a9821c83363e5d816
Stefanos Markidis <stmarkidis@gmail.com>
2021-04-15 20:20:34 +0200

    Upgrade libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
-var webpack_merge = require('webpack-merge');
+var {merge: webpack_merge} = require('webpack-merge');
 
 var base_config = require("@mendix/mendix-hybrid-app-base/webpack.config");
 
-module.exports = function(env) {
-    let config = webpack_merge(base_config(env), {
-    });
-
-    return config;
+module.exports = function (env) {
+    return webpack_merge(base_config(env), {});
 };
